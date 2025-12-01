81121d86f42ae7f48abbcf7245692285c7eb9654
Isabel Cuddihy <104743014+isabelcuddihy@users.noreply.github.com>
2025-12-01 17:53:30 -0500

    Delete .history/client directory

--- .history/client/src/pages/Chatbot+Contact/Chatbot_20251124130450.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export default function Chatbot() {
-  const { currentUser } = useSelector((state: any) => state.accountReducer);
-    return <h2>Page under construction</h2>;}
-
-function useSelector(arg0: (state: any) => any): { currentUser: any; } {
-    throw new Error("Function not implemented.");
-}
